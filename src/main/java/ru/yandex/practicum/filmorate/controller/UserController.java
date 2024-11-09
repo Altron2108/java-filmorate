aa7c1a4a99a38f0529db8c8fb6e0d308ceed5996
Dmitry Yarovoy <altron2108@yandex.ru>
2024-11-09 18:50:33 +0300

    - Добавлены методы для создания, обновления и получения пользователей. - Реализована проверка на существование пользователя перед обновлением, с использованием исключения EntityNotFoundException.

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,10 +1,10 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.EntityNotFoundException;  // Импортируем исключение
 import ru.yandex.practicum.filmorate.model.User;
 
 import jakarta.validation.Valid;
@@ -14,11 +14,12 @@ import java.util.ArrayList;
 @RestController
 @RequestMapping("/users")
 @Validated
+@Slf4j
 public class UserController {
 
-    private static final Logger log = LoggerFactory.getLogger(UserController.class);
     private final List<User> users = new ArrayList<>();
 
+    // Создание пользователя
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public User createUser(@Valid @RequestBody User user) {
@@ -27,20 +28,27 @@ public class UserController {
         return user;
     }
 
+    // Обновление пользователя
     @PutMapping("/{id}")
     public User updateUser(@PathVariable int id, @Valid @RequestBody User user) {
-        User existingUser = users.stream().filter(u -> u.getId() == id).findFirst()
-                .orElseThrow(() -> new RuntimeException("Пользователь не найден"));
+        User existingUser = users.stream()
+                .filter(u -> u.getId() == id)
+                .findFirst()
+                .orElseThrow(() -> new EntityNotFoundException("Пользователь с ID " + id + " не найден"));
+
         existingUser.setEmail(user.getEmail());
         existingUser.setLogin(user.getLogin());
         existingUser.setName(user.getName());
         existingUser.setBirthday(user.getBirthday());
+
         log.info("Пользователь обновлен: {}", existingUser);
         return existingUser;
     }
 
+    // Получение всех пользователей
     @GetMapping
     public List<User> getUsers() {
+        log.info("Запрос на получение всех пользователей");
         return users;
     }
 }
