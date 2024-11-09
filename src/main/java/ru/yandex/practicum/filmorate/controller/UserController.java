6f98d80e658ede376162133437ae6fa6ffe7e02e
Dmitry Yarovoy <altron2108@yandex.ru>
2024-11-09 18:13:10 +0300

    - Реализован контроллер `UserController` с эндпоинтами для создания, обновления и получения пользователей: - `POST /users`: создание нового пользователя с валидацией данных. - `PUT /users/{id}`: обновление данных существующего пользователя по ID. - `GET /users`: получение списка всех пользователей. - Добавлено логирование для операций создания и обновления пользователей.

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,47 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.User;
+
+import jakarta.validation.Valid;
+import java.util.List;
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/users")
+@Validated
+public class UserController {
+
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
+    private final List<User> users = new ArrayList<>();
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public User createUser(@Valid @RequestBody User user) {
+        users.add(user);
+        log.info("Пользователь создан: {}", user);
+        return user;
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable int id, @Valid @RequestBody User user) {
+        User existingUser = users.stream().filter(u -> u.getId() == id).findFirst()
+                .orElseThrow(() -> new RuntimeException("Пользователь не найден"));
+        existingUser.setEmail(user.getEmail());
+        existingUser.setLogin(user.getLogin());
+        existingUser.setName(user.getName());
+        existingUser.setBirthday(user.getBirthday());
+        log.info("Пользователь обновлен: {}", existingUser);
+        return existingUser;
+    }
+
+    @GetMapping
+    public List<User> getUsers() {
+        return users;
+    }
+}
