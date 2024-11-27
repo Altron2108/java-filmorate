ba85cf82c124796d4a1b58901cabd9f215e67144
Dmitry Yarovoy <altron2108@yandex.ru>
2024-11-27 14:05:53 +0300

    !!

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PastOrPresent;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
@@ -13,10 +12,9 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDate;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
+@NoArgsConstructor // Генерирует конструктор по умолчанию
+@AllArgsConstructor // Генерирует конструктор со всеми полями
 public class Film {
-    @NotNull(message = "ID не может быть пустым")
     private Long id;
 
     @NotBlank(message = "Название не может быть пустым")
@@ -26,9 +24,17 @@ public class Film {
     private String description;
 
     @PastOrPresent(message = "Дата выпуска должна быть в прошлом или настоящем")
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd") // Формат даты
     private LocalDate releaseDate;
 
     @Positive(message = "Длительность должна быть положительным числом")
     private int duration;
+
+    // Конструктор с параметрами для инициализации всех полей
+    public Film(String name, String description, LocalDate releaseDate, int duration) {
+        this.name = name;
+        this.description = description;
+        this.releaseDate = releaseDate;
+        this.duration = duration;
+    }
 }
